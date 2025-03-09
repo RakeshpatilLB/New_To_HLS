ba8126c8df2a24c3de3266538ea3139c7bb22b1e
Rakesh <167572033+RakeshpatilLB@users.noreply.github.com>
2025-03-09 18:35:46 +0530

    Update vector_add.cpp

--- projects/vector addition/vector_add.cpp
+++ projects/vector addition/vector_add.cpp
@@ -1,6 +1,8 @@
 #include "vector_add.h"
 
 void vector_add(data_t a[SIZE], data_t b[SIZE], data_t c[SIZE]) {
+    /*These three pragmas specify that arrays a, b, and c should be implemented using a standard RAM interface. 
+    This creates memory ports that can be accessed by external logic.*/
     #pragma HLS INTERFACE ap_memory port=a
     #pragma HLS INTERFACE ap_memory port=b
     #pragma HLS INTERFACE ap_memory port=c
